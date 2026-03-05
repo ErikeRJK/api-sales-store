013c26276242857aca70f59a578b541419c589a4
ErikeRJK <erike.dev@gmail.com>
2026-03-05 19:46:10 -0300

    add configurations and controller autentication jwt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const routesProducts = require("./src/routes/products")
 const routesCategories = require("./src/routes/categories")
 const routesUsers = require("./src/routes/users")
+const routesLogin = require("./src/routes/login")
 require("./src/models")
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/", routesProducts)
 app.use("/", routesCategories)
 app.use(routesUsers)
+app.use(routesLogin)
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
