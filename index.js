486d49586d655ab84eb04db2b246f3b109081244
ErikeRJK <erike.dev@gmail.com>
2026-03-12 18:55:38 -0300

    add configuration cors

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const routesProducts = require("./src/routes/products")
 const routesCategories = require("./src/routes/categories")
 const routesUsers = require("./src/routes/users")
 const routesLogin = require("./src/routes/login")
+const cors = require("cors")
+
 require("./src/models")
 
 const app = express();
 const port = 4505;
 
 app.use(express.json());
+app.use(cors())
 app.use("/", routesProducts)
 app.use("/", routesCategories)
 app.use(routesUsers)
