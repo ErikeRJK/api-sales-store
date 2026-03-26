6c22752830dd646d15088f772ec556769b58fbc9
ErikeRJK <erike.dev@gmail.com>
2026-03-26 20:45:38 -0300

    fix: import sequelize

--- src/models/products_images.js
+++ src/models/products_images.js
@@ -1,4 +1,4 @@
-const sequelize = require("../config/database");
+const sequelize = require("../config/sequelize");
 const { DataTypes } = require("sequelize");
 
 const ProductImages = sequelize.define("ProductsImages", {
