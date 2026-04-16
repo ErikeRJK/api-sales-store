f9885cb0f9046a2ce2f238064c2c21838fa02446
ErikeRJK <erike.dev@gmail.com>
2026-04-16 20:28:55 -0300

    Create helper functions: encrypt user token and add library

---
+++ src/helpers/encrypt-user-token.js
@@ -0,0 +1,20 @@
+const CryptoJS = require("crypto-js");
+
+async function encryptUserToken(userId){
+    try {
+        const encryptUserId = CryptoJS.AES.encrypt(
+            userId,
+            process.env.ENCRYPT_SECRET
+        ).toString
+
+        return encryptUserId
+    } catch (error) {
+        throw new Error(error)
+    }
+
+
+}
+
+module.exports = {
+    encryptUserToken
+}
